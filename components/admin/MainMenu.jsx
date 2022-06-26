89e07da5e87f0183d74a65b6258c039e781ee4e8
Paulo Cardoso <pcardoso.lei@gmail.com>
2022-06-26 14:18:24 +0100

    small changes

--- components/admin/MainMenu.jsx
+++ components/admin/MainMenu.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import personalInformationImage from "../../public/images/mainmenu_1.png";
 import paymentsImage from "../../public/images/mainmenu_2.png";
 import configurationsImage from "../../public/images/mainmenu_3.png";
+import Link from "next/link";
 
 const MainMenu = () => {
   return (
@@ -11,44 +12,61 @@ const MainMenu = () => {
       <div className="mb-7 flex flex-1 justify-center">
         <h5 className="text-3xl font-bold">Conta</h5>
       </div>
-      <div className="my-5 flex cursor-pointer justify-center">
-        <div className="flex w-6/12 shadow-xl">
-          <div>
-            <Image
-              src={personalInformationImage}
-              alt="Informações pessoais"
-              height={96}
-              width={96}
-            />
+      <Link href="/admin/general">
+        <a>
+          <div className="my-5 flex cursor-pointer justify-center">
+            <div className="p-2x flex w-6/12">
+              <div>
+                <Image
+                  src={personalInformationImage}
+                  alt="Informações pessoais"
+                  height={96}
+                  width={96}
+                />
+              </div>
+              <div className="ml-5 mt-6">
+                <div>Informações pessoais</div>
+                <div>Forneça os dados sobre si e como o podemos contactar.</div>
+              </div>
+            </div>
           </div>
-          <div className="ml-5">
-            <div>Informações pessoais</div>
-            <div>Forneça os dados sobre si e como o podemos contactar.</div>
+        </a>
+      </Link>
+      <Link href="/admin/general">
+        <a>
+          <div className="my-5 flex cursor-pointer justify-center">
+            <div className="p-2x flex w-6/12">
+              <div>
+                <Image src={paymentsImage} alt="Informações pessoais" height={96} width={96} />
+              </div>
+              <div className="ml-5 mt-6">
+                <div>Pagamentos e recebimentos</div>
+                <div>Defina como vai receber/pagar as transações que efectuar.</div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="my-5 flex cursor-pointer justify-center">
-        <div className="flex w-6/12 shadow-xl">
-          <div>
-            <Image src={paymentsImage} alt="Informações pessoais" height={96} width={96} />
+        </a>
+      </Link>
+      <Link href="/admin/general">
+        <a>
+          <div className="my-5 flex cursor-pointer justify-center">
+            <div className="p-2x flex w-6/12">
+              <div>
+                <Image
+                  src={configurationsImage}
+                  alt="Informações pessoais"
+                  height={96}
+                  width={96}
+                />
+              </div>
+              <div className="ml-5 mt-6">
+                <div>Configurações</div>
+                <div>Atualize a sua palavra-passe e controle as notificações.</div>
+              </div>
+            </div>
           </div>
-          <div className="ml-5">
-            <div>Pagamentos e recebimentos</div>
-            <div>Defina como vai receber/pagar as transações que efectuar.</div>
-          </div>
-        </div>
-      </div>
-      <div className="my-5 flex cursor-pointer justify-center">
-        <div className="flex w-6/12 shadow-xl">
-          <div>
-            <Image src={configurationsImage} alt="Informações pessoais" height={96} width={96} />
-          </div>
-          <div className="ml-5">
-            <div>Configurações</div>
-            <div>Atualize a sua palavra-passe e controle as notificações.</div>
-          </div>
-        </div>
-      </div>
+        </a>
+      </Link>
     </>
   );
 };
