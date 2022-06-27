1d98c4b655e5870423b1794e3f675ca8279b685e
AdidasBR <rrossellini@hotmail.com>
2022-06-27 14:10:00 +0100

    Blog - Styles

--- components/home/Blog/Blog.jsx
+++ components/home/Blog/Blog.jsx
@@ -1,40 +1,45 @@
 import React from "react";
 
+
 export default function Blog() {
   return (
-    <section className="p-10 lg:p-28">
+    <section className="container mx-auto ">
       <div>
-        <h2 className="mb-3 text-3xl font-bold text-black">Blog</h2>
-        <div className="sec8-card-container">
-          <article className="uplift sec8-article sec8-card1 transition">
-            <h2 className="fs-600 clr-white">Quarto Privado</h2>
-            <p className="fs-200 clr-white">
+        <h2 className="my-10 text-black text-5xl font-bold">Blog</h2>
+        <div className=" grid grid-cols-4 gap-8 ">
+       
+          <article className="p-7 rounded-2xl bg-primary-300 bg-cover bg-center duration-200 ease-in  hover:drop-shadow-xl">
+            <h2 className="text-white mt-64 mb-6 text-2xl">Quarto Privado</h2>
+            <p className="text-white text-base">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e
               que são óptimos investimentos,
             </p>
           </article>
-          <article className="uplift sec8-article sec8-card2 transition">
-            <h2 className="fs-600 clr-white">Quarto Privado</h2>
-            <p className="fs-200 clr-white">
-              {" "}
+         
+          <article className="p-7 rounded-2xl bg-primary-300 bg-cover bg-center duration-200 ease-in  hover:drop-shadow-xl">
+            <h2 className="text-white mt-64 mb-6 text-2xl">Quarto Privado</h2>
+            <p className="text-white text-base">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e
               que são óptimos investimentos,
             </p>
           </article>
-          <article className="uplift sec8-article sec8-card3 transition">
-            <h2 className="fs-600 clr-white">Quarto Privado</h2>
-            <p className="fs-200 clr-white">
+
+          <article className="p-7 rounded-2xl bg-primary-300 bg-cover bg-center duration-200 ease-in  hover:drop-shadow-xl">
+            <h2 className="text-white mt-64 mb-6 text-2xl">Quarto Privado</h2>
+            <p className="text-white text-base">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e
               que são óptimos investimentos,
             </p>
           </article>
-          <article className="uplift sec8-article sec8-card4 transition">
-            <h2 className="fs-600 clr-white">Quarto Privado</h2>
-            <p className="fs-200 clr-white">
+
+          <article className="p-7 rounded-2xl bg-primary-300 bg-cover bg-center duration-200 ease-in  hover:drop-shadow-xl">
+            <h2 className="text-white mt-64 mb-6 text-2xl">Quarto Privado</h2>
+            <p className="text-white text-base">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e
               que são óptimos investimentos,
             </p>
           </article>
+
         </div>
       </div>
     </section>
