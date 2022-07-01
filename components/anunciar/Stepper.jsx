860520cd41924d5e1b0ac28320c248fcb17cf1b2
AdidasBR <rrossellini@hotmail.com>
2022-07-01 17:37:28 +0100

    Stepper

---
+++ components/anunciar/Stepper.jsx
@@ -0,0 +1,139 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Stepper() {
+    return (
+
+        <div className="p-5">
+            <div className="mx-4 p-4">
+                <div className="flex items-center">
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            1
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 1</div>
+                    </div>
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            2
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 2</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            3
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 3</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            4
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 4</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            5
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 5</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            6
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 6</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            7
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 7</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            8
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 8</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 text-center bg-primary-500 border-teal-60">
+                            9
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-primary-500">Passo 9</div>
+                    </div>
+
+                    <div className="flex-auto border-t-2  border-primary-500"></div>
+
+
+
+
+                    {/* PASSO */}
+                    <div className="flex items-center text-white relative">
+                        <div className="rounded-full text-center transition duration-500 ease-in-out h-12 w-12 py-3  bg-terciary-200 ">
+                            10
+                        </div>
+                        <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-teal-600">Passo 10</div>
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+    );
+}
--- pages/anunciar.js
+++ pages/anunciar.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import React from "react";
 
 import FormAnunciar from "../components/anunciar/FormAnunciar";
+import Stepper from "../components/anunciar/Stepper"
 
 
 export default function Anunciar() {
@@ -9,6 +10,7 @@ export default function Anunciar() {
         <>
             <div className="container mx-auto my-20 py-20 rounded-2xl border border-terciary-700">
                 <h1 className="text-6xl  text-center font-bold leading-snug ">Anunciar a sua propriedade é rápido e fácil!</h1>
+                <Stepper />
                 <div>
                     <FormAnunciar />
                 </div>
