eb26a28352a43877bc2e4bd25af56aba5ab7fec1
AdidasBR <rrossellini@hotmail.com>
2022-07-04 20:32:05 +0100

    Estilo Componente QuantityInput

--- components/anunciar/FormPasso2.jsx
+++ components/anunciar/FormPasso2.jsx
@@ -1,7 +1,7 @@
 import { useCurrentStep, useSetCurrentStep } from "../../context/AnunciarProvider";
 import Input from "../utils/Input";
 import Image from "next/image"
-
+import QuantityInput from "../utils/QuantityInput"
 
 const FormPasso2 = () => {
   const currentStep = useCurrentStep();
@@ -16,28 +16,32 @@ const FormPasso2 = () => {
   return (
     <section className="my-20 px-40  w-1/2 ">
       <div className="flex my-8">
-        <Image
-          src="/images/anunciar_capacidade.jpg"
-          alt=""
-          height="25"
-          width="25"
-        ></Image>
+        <div>
+          <Image
+            src="/images/anunciar_capacidade.jpg"
+            alt=""
+            height="25"
+            width="25"
+          ></Image>
+        </div>
         <div className="w-3/5">
           <h2 className="font-bold text-base ml-2  align-middle">Capacidade</h2>
         </div>
-        <div className="">
-          compo
+        <div>
+          <QuantityInput />
         </div>
       </div>
 
 
       <div className="flex my-8">
-        <Image
-          src="/images/anunciar_camas.jpg"
-          alt=""
-          height="25"
-          width="25"
-        ></Image>
+        <div >
+          <Image
+            src="/images/anunciar_camas.jpg"
+            alt=""
+            height="25"
+            width="25"
+          ></Image>
+        </div>
         <div className="w-3/5">
           <h2 className="font-bold text-base ml-2  align-middle">Camas</h2>
         </div>
@@ -48,12 +52,14 @@ const FormPasso2 = () => {
 
 
       <div className="flex my-8">
-        <Image
-          src="/images/anunciar_quartos.jpg"
-          alt=""
-          height="25"
-          width="25"
-        ></Image>
+        <div>
+          <Image
+            src="/images/anunciar_quartos.jpg"
+            alt=""
+            height="25"
+            width="25"
+          ></Image>
+        </div>
         <div className="w-3/5">
           <h2 className="font-bold text-base ml-2  align-middle">Quartos</h2>
         </div>
@@ -65,12 +71,14 @@ const FormPasso2 = () => {
 
 
       <div className="flex my-8">
-        <Image
-          src="/images/anunciar_casas_de_banho.jpg"
-          alt=""
-          height="25"
-          width="25"
-        ></Image>
+        <div>
+          <Image
+            src="/images/anunciar_casas_de_banho.jpg"
+            alt=""
+            height="25"
+            width="25"
+          ></Image>
+        </div>
         <div className="w-3/5">
           <h2 className="font-bold text-base ml-2  align-middle">Casas de Banho</h2>
         </div>
