8aeb98896a3eef02ccd2275981950d67d78f4c4f
AdidasBR <rrossellini@hotmail.com>
2022-07-12 21:45:39 +0100

    Correção Espaçamento

--- components/destaques/RoomInformation/RoomsSemelhantes/RoomsSemelhantes.jsx
+++ components/destaques/RoomInformation/RoomsSemelhantes/RoomsSemelhantes.jsx
@@ -8,23 +8,23 @@ export default function RoomSemelhantes() {
     return (
         <section className="mb-40">
             <div className="font-bold text-2xl mb-5">Casas semelhantes</div>
-            <div className="grid w-3/5 lg:grid-cols-4 justify-start gap-48 mb-10">
-                <article className="relative h-56 w-56  rounded-lg  bg-destaques-slider1 ">
+            <div className="grid w-3/5 lg:grid-cols-4 justify-start gap-52 mb-10">
+                <article className="relative h-56 w-48  rounded-lg  bg-destaques-slider1 ">
                     <h2 className=" text-base text-white p-3 mt-3">Quarto Privado</h2>
                     <p className="bold absolute bottom-3 right-4 text-2xl text-white font-bold">320&euro;/mês</p>
                 </article>
 
-                <article className="relative h-56 w-56  rounded-lg bg-destaques-slider2">
+                <article className="relative h-56 w-48  rounded-lg bg-destaques-slider2">
                     <h2 className=" text-base text-white p-3 mt-3">Quarto partilhado</h2>
                     <p className="bold absolute bottom-3 right-4 text-2xl text-white font-bold">320&euro;/mês</p>
                 </article>
 
-                <article className="relative h-56 w-56  rounded-lg bg-destaques-slider2">
+                <article className="relative h-56 w-48  rounded-lg bg-destaques-slider2">
                     <h2 className=" text-base text-white p-3 mt-3">Casa Inteira</h2>
                     <p className="bold absolute bottom-3 right-4 text-2xl text-white font-bold">320&euro;/mês</p>
                 </article>
 
-                <article className="relative h-56 w-56  rounded-lg bg-destaques-slider2">
+                <article className="relative h-56 w-48  rounded-lg bg-destaques-slider2">
                     <h2 className=" text-base text-white p-3 mt-3">Quarto Privado</h2>
                     <p className="bold absolute bottom-3 right-4 text-2xl text-white font-bold">320&euro;/mês</p>
                 </article>
