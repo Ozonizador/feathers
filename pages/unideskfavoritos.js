db27695738ca72e95c8dc16b3f295c3011dcd85c
AdidasBR <rrossellini@hotmail.com>
2022-07-16 19:06:36 +0100

    Unidesk Favoritos

---
+++ pages/unideskfavoritos.js
@@ -0,0 +1,11 @@
+import UnideskFavoritos from "../components/unidesk/Estudante/favourites/UnideskFavoritos";
+
+export default function Home() {
+    return (
+        <div>
+            <div>
+                <UnideskFavoritos />
+            </div>
+        </div>
+    );
+}
