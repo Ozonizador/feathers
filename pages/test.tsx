9bd19e6d6698beb43ff4da9b463b8dc401694045
AdidasBR <rrossellini@hotmail.com>
2022-07-20 21:11:22 +0100

    Inicio Reviews

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,10 +2,15 @@ import ModalAlterarReserva from "../components/modals/ModalAlteralReserva";
 import ModalAvaliarExperiencia from "../components/modals/ModalAvaliarExperiencia";
 import ModalDenuncia from "../components/modals/ModalDenuncia";
 // import ModalAvaliarExperiencia from "../components/modals/ModalAvaliarExperiencia";
+// import Menu from "../components/unidesk/Menus/MenuSenhorio";
+// import Menu from "../components/unidesk/Menus/MenuEstudante";
+import Menu from "../components/unidesk/Menus/HospedesMenu";
+
 const Index = (): JSX.Element => {
   return (
     <div className="flex flex-1 justify-center p-10">
-      <ModalAvaliarExperiencia defaultOpen={true} />
+      {/* <ModalAvaliarExperiencia defaultOpen={true} /> */}
+      {/* <Menu /> */}
       {/* <ModalDenuncia defaultOpen={false} /> */}
       {/* <ModalAlterarReserva defaultOpen={true} /> */}
     </div>
