a7380ead11e6aa20992c5d22a2f1d2c90e53ba1b
Paulo Cardoso <pcardoso.lei@gmail.com>
2022-07-25 01:43:25 +0100

    fix link

--- components/Stay/Info/StayInfo.tsx
+++ components/Stay/Info/StayInfo.tsx
@@ -40,9 +40,9 @@ const StayInfo = () => {
           </div>
         </div>
 
-        <div className="flex flex-1 flex-col items-center">
-          <Link href="/unidesk/inbox">
-            <a>
+        <Link href="/unidesk/inbox">
+          <a>
+            <div className="flex flex-1 flex-col items-center">
               <div>
                 <RiMailSendLine className="mb-2 text-4xl text-green-400" />
               </div>
@@ -51,9 +51,9 @@ const StayInfo = () => {
                 <br />
                 mensagem
               </div>
-            </a>
-          </Link>
-        </div>
+            </div>{" "}
+          </a>
+        </Link>
 
         <div className="flex flex-1 cursor-pointer flex-col items-center" onClick={() => openModalAlterarReserva()}>
           <div>
