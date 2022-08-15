f5f5cecbeaf4f4438547c825a730b1a618601885
AdidasBR <rrossellini@hotmail.com>
2022-08-15 19:09:00 +0100

    Anúncio Mobile

--- components/destaques/RoomInformation/RoomSenhorio/RoomSenhorio.tsx
+++ components/destaques/RoomInformation/RoomSenhorio/RoomSenhorio.tsx
@@ -14,8 +14,15 @@ export default function RoomSenhorio() {
     <section className="my-20">
       <div className="mb-5 text-2xl font-bold">Sobre o seu senhorio</div>
 
-      <div className="flex w-full flex-col items-end gap-8 lg:w-11/12 lg:flex-row">
-        <div className="w-full lg:max-w-md">
+      <div className="block max-w-md lg:hidden">
+        <div>
+          <h1 className="mb-4 text-2xl font-bold">Olá, sou a {advertisement.host.name}!</h1>
+          <p className="text-secondary-400">{advertisement.host.description}</p>
+        </div>
+      </div>
+
+      <div className="flex w-full flex-col gap-8 lg:w-11/12 lg:flex-row lg:items-end">
+        <div className="max-w-md">
           <div className="w-full lg:w-64">
             <Card>
               <div className="flex justify-end px-4 pt-4"></div>
@@ -36,13 +43,22 @@ export default function RoomSenhorio() {
                 <span className="text-sm text-gray-500 dark:text-gray-400">{`${
                   advertisement.host.gender === Gender.female ? "Senhoria" : "Senhorio"
                 } desde ${new Date(advertisement.host.createdAt).getFullYear()}`}</span>
+                <hr />
+                <div className="mt-4 flex items-center space-x-3 lg:mt-6">
+                  <div className="">
+                    <RiMailSendFill className=" text-4xl text-blue-600 " />
+                  </div>
+                  <div className="">
+                    Taxa de resposta <br /> 90%
+                  </div>
+                </div>
               </div>
             </Card>
           </div>
         </div>
 
         <div className="flex w-full flex-col lg:w-7/12">
-          <div className="max-w-md">
+          <div className="hidden max-w-md lg:block">
             <div>
               <h1 className="mb-4 text-2xl font-bold">Olá, sou a {advertisement.host.name}!</h1>
               <p className="text-secondary-400">{advertisement.host.description}</p>
