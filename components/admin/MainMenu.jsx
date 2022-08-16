42d7a4fbb55ffe93617b728d3b1b73bc8188091c
AdidasBR <rrossellini@hotmail.com>
2022-08-16 12:13:22 +0100

    Admin - Estilos para Desktop e Mobile

--- components/admin/MainMenu.jsx
+++ components/admin/MainMenu.jsx
@@ -14,58 +14,69 @@ const MainMenu = () => {
   return (
     <>
       <div className="mb-7 flex flex-1 justify-center">
-        <h5 className="text-3xl font-bold">Conta</h5>
+        <h5 className="text-5xl font-bold mt-16 mb-12">Conta</h5>
       </div>
-      <Link href="/admin/general">
-        <a>
-          <div className="my-5 flex cursor-pointer justify-center">
-            <div className="p-2x flex">
-              <div>
-                <Image
-                  src={personalInformationImage}
-                  alt="Informações pessoais"
-                  height={96}
-                  width={96}
-                />
+
+      <div className="px-4 flex flex-col justify-center align-middle items-center gap-12">
+
+        <div className="lg:w-1/3 drop-shadow-xl bg-white rounded-2xl">
+          <Link href="/admin/general">
+            <a>
+              <div className="my-5 flex cursor-pointer pl-5 ">
+                <div className="flex">
+                  <div>
+                    <Image src={personalInformationImage} alt="Informações pessoais" height={96} width={96} />
+
+                  </div>
+                  <div className="ml-3 lg:ml-5 lg:mt-6 p-2">
+                    <div className="font-bold text-xl lg:text-2xl mb-2">Informações pessoais</div>
+                    <div>Forneça os dados sobre si e como o podemos contactar.</div>
+                  </div>
+                </div>
               </div>
-              <div className="ml-5 mt-6">
-                <div>Informações pessoais</div>
-                <div>Forneça os dados sobre si e como o podemos contactar.</div>
+            </a>
+          </Link>
+        </div>
+
+
+        <div className="lg:w-1/3 drop-shadow-xl bg-white rounded-2xl">
+          <Link href="/admin/payments">
+            <a>
+              <div className="my-5 flex cursor-pointer pl-5 ">
+                <div className=" flex">
+                  <div>
+                    <Image src={paymentsImage} alt="Pagamentos" height={96} width={96} />
+                  </div>
+                  <div className="ml-3 lg:ml-5 lg:mt-6 p-2">
+                    <div className="font-bold text-xl lg:text-2xl mb-2">Pagamentos e recebimentos</div>
+                    <div>Defina como vai receber/pagar as transações que efectuar.</div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </a>
-      </Link>
-      <Link href="/admin/payments">
-        <a>
-          <div className="my-5 flex cursor-pointer justify-center">
-            <div className="p-2x flex">
-              <div>
-                <Image src={paymentsImage} alt="Pagamentos" height={96} width={96} />
+            </a>
+          </Link>
+        </div>
+
+
+        <div className="lg:w-1/3 drop-shadow-xl bg-white rounded-2xl">
+          <Link href="/admin/configurations">
+            <a>
+              <div className="my-5 flex cursor-pointer pl-5 ">
+                <div className=" flex">
+                  <div>
+                    <Image src={configurationsImage} alt="Configurações" height={96} width={96} />
+                  </div>
+                  <div className="ml-3 lg:ml-5 lg:mt-6 p-2">
+                    <div className="font-bold text-xl lg:text-2xl mb-2">Configurações</div>
+                    <div>Atualize a sua palavra-passe e controle as notificações.</div>
+                  </div>
+                </div>
               </div>
-              <div className="ml-5 mt-6">
-                <div>Pagamentos e recebimentos</div>
-                <div>Defina como vai receber/pagar as transações que efectuar.</div>
-              </div>
-            </div>
-          </div>
-        </a>
-      </Link>
-      <Link href="/admin/configurations">
-        <a>
-          <div className="my-5 flex cursor-pointer justify-center">
-            <div className="p-2x flex">
-              <div>
-                <Image src={configurationsImage} alt="Configurações" height={96} width={96} />
-              </div>
-              <div className="ml-5 mt-6">
-                <div>Configurações</div>
-                <div>Atualize a sua palavra-passe e controle as notificações.</div>
-              </div>
-            </div>
-          </div>
-        </a>
-      </Link>
+            </a>
+          </Link>
+        </div>
+      </div>
+
     </>
   );
 };
