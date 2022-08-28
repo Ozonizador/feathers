fd2f4d74ccd834110f1620fdeb135219ca13edaf
AdidasBR <rrossellini@hotmail.com>
2022-08-28 16:23:00 +0100

    Margin Top - IMG Testemunhos.

--- components/home/homeSection6Cards/HomeSection6Cards.jsx
+++ components/home/homeSection6Cards/HomeSection6Cards.jsx
@@ -8,7 +8,7 @@ export default function HomeSection6Cards({ img, testimonial, name, desc }) {
         <Image height={32} width={32} src="/images/icon-quotest.svg" alt="" style={{ width: "42px" }} />
         <p className="text-xl">{testimonial}</p>
       </div>
-      <div>
+      <div className="mt-10">
         <Image height={64} width={64} src={img} alt="" />
         <div>
           <p className="mb-0 text-xl font-bold">{name}</p>
