be0dbfe3097cbdefd649952fd55d9c5a2067dcd1
AdidasBR <rrossellini@hotmail.com>
2022-09-03 16:00:44 +0100

    Correção Blog Cards para mobile e Logo Footer

--- components/home/Blog/Blog.jsx
+++ components/home/Blog/Blog.jsx
@@ -6,12 +6,12 @@ export default function Blog() {
       <div>
         <h2 className="my-10 text-5xl font-bold text-black">Blog</h2>
         <div className=" grid gap-8 lg:grid-cols-4">
-          <article className="rounded-2xl bg-primary-300 h-96 w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
+          <article className="rounded-2xl bg-primary-300 h-96 w-full lg:w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
             <h2 className="mt-60 mb-6 text-2xl text-white">5 dicas importantes para os senhorios</h2>
 
           </article>
 
-          <article className="rounded-2xl bg-primary-300 h-96 w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
+          <article className="rounded-2xl bg-primary-300 h-96 w-full  lg:w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
             <h2 className="mt-36 mb-6 text-2xl text-white">Quarto Privado</h2>
             <p className="text-base text-white">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e que são óptimos
@@ -19,7 +19,7 @@ export default function Blog() {
             </p>
           </article>
 
-          <article className="rounded-2xl bg-primary-300 h-96 w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
+          <article className="rounded-2xl bg-primary-300 h-96 w-full lg:w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
             <h2 className="mt-36 mb-6 text-2xl text-white">Quarto Privado</h2>
             <p className="text-base text-white">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e que são óptimos
@@ -27,7 +27,7 @@ export default function Blog() {
             </p>
           </article>
 
-          <article className="rounded-2xl bg-primary-300 h-96 w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
+          <article className="rounded-2xl bg-primary-300 h-96 w-full lg:w-64 bg-cover bg-center p-7 duration-200 ease-in  hover:drop-shadow-xl">
             <h2 className="mt-36 mb-6 text-2xl text-white">Quarto Privado</h2>
             <p className="text-base text-white">
               O mercado imobiliário e o de arrendamento são dois sectores que andam de mão dada e que são óptimos
--- components/home/homeSection7/HomeSection7.jsx
+++ components/home/homeSection7/HomeSection7.jsx
@@ -6,12 +6,12 @@ export default function HomeSection7() {
   return (
     <section>
       <div className="my-24 rounded-3xl bg-terciary-300 lg:w-full">
-        <div className="flex flex-col-reverse lg:flex-row">
+        <div className="flex flex-col-reverse lg:flex-row lg:h-96">
           <div className="items-center p-0 text-center align-middle lg:basis-2/4 lg:p-0 lg:text-left lg:ml-12">
             <h2 className="mt-8 py-8 text-2xl font-bold text-secondary-500 lg:mt-8 lg:ml-6 lg:text-5xl ">
               Tem Uma<br /> Propriedade?
             </h2>
-            <div className="lg:pb-0 text-2xl text-terciary-400">
+            <div className="lg:pb-0 text-2xl text-terciary-400 -mb-6">
               <div className="hidden lg:mt-2 lg:ml-6 lg:block">
                 Comece já a anunciar e rentabilize
                 <br />o seu imóvel!
@@ -19,9 +19,9 @@ export default function HomeSection7() {
 
               <div className="block lg:hidden">Comece já a anunciar e rentabilize o seu imóvel!</div>
             </div>
-            <div className="mb-14 mt-8 lg:mb-20 lg:mt-0 lg:pt-10">
+            <div className="mb-14 mt-8 lg:mb-20 lg:mt-0 lg:pt-16">
               <Link href="/anunciar">
-                <a className="rounded-full bg-primary-500 py-4 px-16 lg:px-7 text-white duration-200 ease-in hover:bg-primary-500 hover:text-white hover:drop-shadow-xl lg:ml-6">
+                <a className="rounded-full bg-primary-500 py-4 px-16 lg:px-9  text-white duration-200 ease-in hover:bg-primary-500 hover:text-white hover:drop-shadow-xl lg:ml-6">
                   Anunciar
                 </a>
               </Link>
@@ -30,7 +30,7 @@ export default function HomeSection7() {
           <div className="lg:min-h-96 h-96 w-full rounded-3xl lg:basis-2/4">
             <div className="relative h-full w-full">
               <div className="absolute h-full w-full">
-                <Image src="/images/img9.jpg" layout="fill" alt="" className="rounded-3xl"></Image>
+                <Image src="/images/house-key.jpg" layout="fill" alt="" className="rounded-3xl"></Image>
               </div>
               <div className="absolute right-20  bottom-10  mb-6 lg:right-8 lg:bottom-6">
                 <Link href="/funciona">
