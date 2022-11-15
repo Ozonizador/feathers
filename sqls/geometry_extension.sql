10dcf2c3681d4382b296abd41e4913b1492fd50e
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2022-11-15 23:43:34 +0000

    pushing version up

--- sqls/geometry_extension.sql
+++
@@ -1,3 +0,0 @@
-create EXTENSION postgis schema extensions;
-
-SELECT AddGeometryColumn ('public','advertisements','geom',4326,'POINT',2);
--- supabase/migrations/20221003212343_average_per_host.sql
+++ supabase/migrations/20221003212343_average_per_host.sql
@@ -1,3 +1,3 @@
-create or replace function average_per_host(host uuid) returns int as $$
+create or replace function public.average_per_host(host uuid) returns int as $$
    select AVG("overall_rating") FROM reviews JOIN stays on stays.id = reviews.stay_id JOIN advertisements on advertisements.id = stays.advertisement_id WHERE advertisements.host_id = host
 $$ language sql;
