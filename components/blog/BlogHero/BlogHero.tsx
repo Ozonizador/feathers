abe7084373715abd9b6455effce95b92dbf72c21
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2022-11-15 20:25:21 +0000

    small padding fix

--- components/blog/BlogHero/BlogHero.tsx
+++ components/blog/BlogHero/BlogHero.tsx
@@ -14,9 +14,9 @@ export default function BlogHero({ blogs }: BlogHeroProps) {
           blogs.map((blog, index) => {
             return (
               <article className=" relative h-96 w-full  rounded-3xl  bg-black px-7 lg:w-2/6" key={index}>
-                <Image src={blog.image} layout="fill" alt="" className="opacity-50" />
+                <Image src={blog.image} layout="fill" alt="" className="rounded-3xl opacity-50" />
 
-                <div className="absolute bottom-8 left-4 w-11/12 lg:left-6">
+                <div className="absolute bottom-8 left-4 w-11/12">
                   <h2 className="bold  mb-2 text-2xl font-normal capitalize text-white">{blog.title}</h2>
                   <p className="bold  text-base font-normal normal-case text-white line-clamp-2">{blog.description}</p>
                   <div className="mt-8 flex flex-row justify-between">
