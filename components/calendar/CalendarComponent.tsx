217c3b5a948b50f341bbe6c3c9e1c272c2996a5f
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-06-04 10:56:58 +0100

    adding calendar component. missing work here

---
+++ components/calendar/CalendarComponent.tsx
@@ -0,0 +1,45 @@
+import { format, subHours, startOfMonth } from "date-fns";
+import {
+  MonthlyBody,
+  MonthlyDay,
+  MonthlyCalendar,
+  MonthlyNav,
+  DefaultMonthlyEventItem,
+} from "@zach.codes/react-calendar";
+import { useState } from "react";
+import { pt } from "date-fns/locale";
+
+type EventType = {
+  title: string;
+  date: Date;
+};
+
+export const CalendarComponent = () => {
+  let [currentMonth, setCurrentMonth] = useState<Date>(startOfMonth(new Date()));
+
+  return (
+    <MonthlyCalendar currentMonth={currentMonth} onCurrentMonthChange={(date) => setCurrentMonth(date)} locale={pt}>
+      <MonthlyNav />
+      <MonthlyBody
+        events={[
+          { title: "Call John", date: subHours(new Date(), 2) },
+          { title: "Call John", date: subHours(new Date(), 1) },
+          { title: "Meeting with Bob", date: new Date() },
+        ]}
+      >
+        <MonthlyDay<EventType>
+          renderDay={(data) =>
+            data.map((item, index) => (
+              <DefaultMonthlyEventItem
+                key={index}
+                title={item.title}
+                // Format the date here to be in the format you prefer
+                date={format(item.date, "k:mm")}
+              />
+            ))
+          }
+        />
+      </MonthlyBody>
+    </MonthlyCalendar>
+  );
+};
--- pages/unidesk/senhorio/calendar.tsx
+++ pages/unidesk/senhorio/calendar.tsx
@@ -1,12 +1,40 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
 import React from "react";
+import { CalendarComponent } from "../../../components/calendar/CalendarComponent";
 import MenuSenhorio from "../../../components/unidesk/Menus/MenuSenhorio";
 import { UnideskStructure } from "../../../components/unidesk/UnideskStructure";
 import Button from "../../../components/utils/Button";
 import Input from "../../../components/utils/Input";
 
-/* todo */
+const ESTADIA_MINIMA = [
+  { label: "3 Meses", value: 3 },
+  { label: "4 Meses", value: 4 },
+  { label: "5 Meses", value: 5 },
+  { label: "6 Meses", value: 6 },
+  { label: "7 Meses", value: 7 },
+  { label: "8 Meses", value: 8 },
+  { label: "9 Meses", value: 9 },
+  { label: "10 Meses", value: 10 },
+  { label: "11 Meses", value: 11 },
+  { label: "12 Meses", value: 12 },
+];
+
+const TEMPO_ANTECEDENCIA = [
+  { label: "1 Meses", value: 1 },
+  { label: "2 Meses", value: 2 },
+  { label: "3 Meses", value: 3 },
+  { label: "4 Meses", value: 4 },
+  { label: "5 Meses", value: 5 },
+  { label: "6 Meses", value: 6 },
+  { label: "7 Meses", value: 7 },
+  { label: "8 Meses", value: 8 },
+  { label: "9 Meses", value: 9 },
+  { label: "10 Meses", value: 10 },
+  { label: "11 Meses", value: 11 },
+  { label: "12 Meses", value: 12 },
+];
+
 const calendar = () => {
   return (
     <UnideskStructure>
@@ -14,48 +42,54 @@ const calendar = () => {
         <MenuSenhorio />
       </UnideskStructure.Menu>
       <UnideskStructure.Content>
-        <div className="ml-6 w-11/12 lg:ml-10 lg:w-2/5">
-          <div className=" mb-6  flex flex-col justify-between lg:flex-row lg:items-center lg:align-middle">
-            <label className="mt-2 mb-2 block text-base lg:mb-0">Estadia mínima</label>
-            <select className="h-12 w-full rounded-md border border-solid border-terciary-500 bg-white py-2 px-3 lg:w-60 ">
-              <option>Idioma</option>
-              <option>Casa</option>
-              <option>Apartamento</option>
+        <div className="-ml-4 w-full">
+          <CalendarComponent />
+        </div>
+        <div className="mt-5 flex flex-col gap-4">
+          <div className="mb-6 flex flex-col lg:flex-row lg:items-center lg:align-middle">
+            <label className="mb-2 mt-2 block text-base lg:mb-0">Estadia mínima</label>
+            <select className="ml-20 h-12 w-full rounded-md border border-solid border-terciary-500 bg-white px-3 py-2 lg:w-60">
+              {ESTADIA_MINIMA.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
 
-          <div className=" mb-6  flex flex-col justify-between lg:flex-row lg:items-center lg:align-middle">
-            <label className="mt-2 mb-2 block text-base lg:mb-0">Tempo de antecedência</label>
-            <select className="h-12 w-full rounded-md border border-solid border-terciary-500 bg-white py-2 px-3 lg:w-60 ">
-              <option>Idioma</option>
-              <option>Casa</option>
-              <option>Apartamento</option>
+          <div className="mb-6 flex flex-col lg:flex-row lg:items-center lg:align-middle">
+            <label className="mb-2 mt-2 block text-base lg:mb-0">Tempo de antecedência</label>
+            <select className="ml-4 h-12 w-full rounded-md border border-solid border-terciary-500 bg-white px-3 py-2 lg:w-60">
+              {TEMPO_ANTECEDENCIA.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
             </select>
           </div>
 
-          <div className="pb-4">
+          <div className="w-96 pb-4">
             <Button onClick={() => {}} type="button">
               Guardar alterações
             </Button>
           </div>
 
-          <div className="mt-10 mb-8 text-2xl font-bold">Descontos (opcional)</div>
-
-          <div>
-            <div className=" mb-3  flex flex-col justify-between lg:flex-row lg:items-center lg:align-middle">
-              <label className="mt-2 mb-2 block text-base lg:mb-0">Tempo de antecedência</label>
-              <div className="  w-full lg:w-60">
-                <Input label={""} labelText="" customCss="percent" onChange={() => {}} value="" />
+          <div className="mb-5 mt-4 flex flex-col gap-4">
+            <div className="mt-4 text-2xl font-bold">Descontos (opcional)</div>
+            <div className="flex flex-col lg:flex-row lg:items-center lg:align-middle">
+              <label className="block text-base lg:mb-0">Desconto trimestral</label>
+              <div className="ml-12 w-full lg:w-20">
+                <Input label={""} labelText="" customCss="percent" onChange={() => {}} value={0} />
               </div>
             </div>
 
-            <div className="mb-6 flex flex-col justify-between lg:flex-row lg:items-center lg:align-middle">
-              <label className="mt-2 mb-2 block text-base lg:mb-0">Desconto semestral</label>
-              <div className="  w-full lg:w-60">
-                <Input label={""} value="" onChange={() => {}} labelText="" customCss="percent" />
+            <div className="flex flex-col lg:flex-row lg:items-center lg:align-middle">
+              <label className="block text-base lg:mb-0">Desconto semestral</label>
+              <div className="ml-12 w-full lg:w-20">
+                <Input label={""} value={0} onChange={() => {}} labelText="" customCss="percent" />
               </div>
             </div>
-            <div className="mb-14 pb-4">
+            <div className="w-96 pb-4">
               <Button onClick={() => {}} type="button">
                 Guardar alterações
               </Button>
