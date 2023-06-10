b56d896244e660618e5c5443b226067543adaeb7
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-06-10 14:30:44 +0100

    adding more logic

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -12,7 +12,8 @@ INSERT INTO "public"."profiles" ("id", "slug", "name", "surname", "nationality",
 
 -- advertisements
 INSERT INTO "public"."advertisements" ("id", "slug", "type", "place", "street", "street_number", "floor", "postal_code", "rooms", "beds", "tenant_number", "bathrooms", "title", "description", "type_flex_host", "type_host", "photos", "house_rules", "month_rent", "extra_per_host", "guarantee_value", "expenses", "available", "host_lives_property", "verified", "host_id", "geom", "agreementsinfo", "livingroom_amenities", "bedroom_amenities") 
-VALUES ('0a54f7fe-2fda-4d30-9767-954796daa5bd','aaaa', 'ENTIRE_SPACE', 'Nazare', 'Rua de Teste', '1', '1', '9999-111', 1, 1, 1, 1, 'Test de Anuncio', 'Description de Anuncio', 'FLEX', 'PROFISSIONAL', '[]', '{}', 300, 80, 100, '{}', 'AVAILABLE', false, false, '3a21df80-34be-435a-8c61-06da3c9e1caa', null, '{}', '{SOFA, BED}', '{SOFA}');
+VALUES ('0a54f7fe-2fda-4d30-9767-954796daa5bd','aaaa', 'ENTIRE_SPACE', 'Nazare', 'Rua de Teste', '1', '1', '9999-111', 1, 1, 1, 1, 'Test de Anuncio', 'Description de Anuncio', 'FLEX', 'PROFISSIONAL', '[]', '{}', 300, 80, 100, '{}', 'AVAILABLE', false, false, '3a21df80-34be-435a-8c61-06da3c9e1caa', null, '{}', '{SOFA, BED}', '{SOFA}'),
+('ab723c81-b13b-47b6-867c-091377e9b418', 'bbbb', 'ENTIRE_SPACE', 'Porto', 'Rua de Teste SATAY', '2', '1', '0000-111', 1, 1, 2, 3, 'Teste de Estadia', 'Descricao', 'FLEX','PROFISSIONAL', '[]', '{}', 400, 30, 50, '{}', 'AVAILABLE', false, false, 'ab723c81-b13b-47b6-867c-091377e9b418', null, '{}', '{}', '{}');
 
 
 -- reservations
@@ -20,12 +21,17 @@ INSERT INTO "public"."reservations" ("id", "start_date", "end_date", "advertisem
 ('bce866e4-109d-4ca3-852e-1b651190349e', '2023-02-03', '2023-02-03', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'REQUESTED', 'c4c7874c-a388-46c1-94db-5cb11839ff7b'),
 ('4b6d0fef-d4f0-4d5b-9e04-c4e92a83169a', '2023-03-04', '2023-03-05', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'REJECTED', 'ab723c81-b13b-47b6-867c-091377e9b418'),
 ('629e0125-dd17-46a5-ad3b-11de8f4e1bd6', '2032-05-01', '2032-08-01', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'ACCEPTED', 'ab723c81-b13b-47b6-867c-091377e9b418'),
-('53d879c4-23b4-48a7-a89c-f5d25b675db3', '2023-01-01', '2023-12-31', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'ACCEPTED', 'c4c7874c-a388-46c1-94db-5cb11839ff7b');
+('53d879c4-23b4-48a7-a89c-f5d25b675db3', '2023-01-01', '2023-12-31', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'ACCEPTED', 'c4c7874c-a388-46c1-94db-5cb11839ff7b'),
+('e899eaeb-0618-4db9-8fe9-3717de5649ad', '2023-01-01', '2025-12-31', 'ab723c81-b13b-47b6-867c-091377e9b418', 'ACCEPTED', '3a21df80-34be-435a-8c61-06da3c9e1caa');
+
 
 -- stays
 INSERT INTO "public"."stays" ("id", "advertisement_id", "tenant_id", "reservation_id", "status") VALUES
 ('d977b25a-855d-4707-bb12-ca9972abae8e', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'ab723c81-b13b-47b6-867c-091377e9b418', '629e0125-dd17-46a5-ad3b-11de8f4e1bd6','OK'),
-('0dc9f1a2-566d-4211-92b8-0acee274f595', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'c4c7874c-a388-46c1-94db-5cb11839ff7b', '53d879c4-23b4-48a7-a89c-f5d25b675db3', 'OK');
+('0dc9f1a2-566d-4211-92b8-0acee274f595', '0a54f7fe-2fda-4d30-9767-954796daa5bd', 'c4c7874c-a388-46c1-94db-5cb11839ff7b', '53d879c4-23b4-48a7-a89c-f5d25b675db3', 'OK'),
+('e899eaeb-0618-4db9-8fe9-3717de5649ad', 'ab723c81-b13b-47b6-867c-091377e9b418', '3a21df80-34be-435a-8c61-06da3c9e1caa', 'e899eaeb-0618-4db9-8fe9-3717de5649ad', 'OK');
+
+
 
 -- reviews
 INSERT INTO "public"."reviews" ("stay_id", "overall_rating", "location_rating", "value_quality_rating", "landlord_rating", "comodities_rating", "public_review", "private_review") VALUES
