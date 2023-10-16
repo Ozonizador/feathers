e45706c74fd976ba90dbc733689012327bb641e5
Rafael Norte <rnrfnorte@hotmail.com>
2023-10-16 17:54:44 +0100

    Added gallery

--- components/destaques/RoomInformation/SingleRoomGrid/SingleRoomGrid.tsx
+++ components/destaques/RoomInformation/SingleRoomGrid/SingleRoomGrid.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { useGetSingleAdvertisement } from "../../../../context/ShowingSingleAdvertisementProvider";
+import Gallery from "../../../gallery/gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import { number } from "zod";
 
 export const SingleRoomGrid = () => {
   const advertisement = useGetSingleAdvertisement();
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const slideImages = () => {
+    let slideImages: { src: string }[] = [];
+    advertisement?.photos.map((photo) => {
+      slideImages.push({
+        src: photo.url,
+      });
+    });
+
+    console.log("test");
+    return slideImages;
+  };
+
+  const handelClick = (index: number) => {
+    setOpen(true);
+    setIndex(index);
+  };
+
   return (
     <section className="my-8 w-full">
       {(advertisement?.photos === null || advertisement?.photos.length === 0) && (
@@ -13,39 +38,71 @@ export const SingleRoomGrid = () => {
         <div className="grid grid-cols-12 gap-2">
           <div className="relative col-span-6 row-span-2 h-96">
             {advertisement.photos[0] ? (
-              <Image src={advertisement.photos[0].url} alt="no photo available" fill style={{ objectFit: "cover" }} />
+              <Image
+                src={advertisement.photos[0].url}
+                alt="no photo available"
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handelClick(0)}
+              />
             ) : (
               <></>
             )}
           </div>
           <div className="relative col-span-2">
             {advertisement.photos[1] ? (
-              <Image src={advertisement.photos[1].url} alt="no photo available" fill style={{ objectFit: "cover" }} />
+              <Image
+                src={advertisement.photos[1].url}
+                alt="no photo available"
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handelClick(1)}
+              />
             ) : (
               <></>
             )}
           </div>
           <div className="relative col-span-4">
             {advertisement.photos[2] ? (
-              <Image src={advertisement.photos[2].url} alt="no photo available" fill style={{ objectFit: "cover" }} />
+              <Image
+                src={advertisement.photos[2].url}
+                alt="no photo available"
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handelClick(2)}
+              />
             ) : (
               <></>
             )}
           </div>
           <div className="relative col-span-4">
             {advertisement.photos[3] ? (
-              <Image src={advertisement.photos[3].url} alt="no photo available" fill style={{ objectFit: "cover" }} />
+              <Image
+                src={advertisement.photos[3].url}
+                alt="no photo available"
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handelClick(3)}
+              />
             ) : (
               <></>
             )}
           </div>
           <div className="relative col-span-2">
             {advertisement.photos[4] ? (
-              <Image src={advertisement.photos[4].url} alt="no photo available" fill style={{ objectFit: "cover" }} />
+              <Image
+                src={advertisement.photos[4].url}
+                alt="no photo available"
+                fill
+                style={{ objectFit: "cover" }}
+                onClick={() => handelClick(4)}
+              />
             ) : (
               <></>
             )}
           </div>
+
+          <Lightbox open={open} close={() => setOpen(false)} plugins={[Zoom]} index={index} slides={slideImages()} />
         </div>
       )}
     </section>
