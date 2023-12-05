f8a0e35699eae140848dac7cf8add5b1ac109276
rajshastri03 <developer.designer18@gmail.com>
2023-12-05 11:45:55 +0530

    Implemented stepper for mobile view and adjusted city text

--- components/anunciar/Stepper.tsx
+++ components/anunciar/Stepper.tsx
@@ -2,10 +2,24 @@ import classNames from "classnames";
 import React from "react";
 import { useCurrentStep } from "../../context/AnunciarProvider";
 import { useTranslation } from "next-i18next";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 export default function Stepper() {
   const { t } = useTranslation();
   const currentStep = useCurrentStep();
+
+  const steps = Array.from({ length: 9 }, (_, index) => index +1);
+  function chunkArray(array: number[], size: number) {
+    return Array.from({ length: Math.ceil(array.length / size) }, (v, i) =>
+      array.slice(i * size, i * size + size)
+    );
+  }
+  const chunk = chunkArray(steps, 3)
+  // console.log(steps,'step')
+  // console.log(chunk, 'chunkss')
+  // console.log('current', currentStep)
+  let chunkIndex
+  currentStep <= 2 ? chunkIndex = 0 : ((currentStep > 2 && currentStep <= 5 )? chunkIndex = 1: chunkIndex=2)
   return (
     <>
       {/* DESKTOP */}
@@ -14,86 +28,48 @@ export default function Stepper() {
           <div className="flex items-center justify-center">
             <div className="flex-auto border-t-2 border-primary-500"></div>
 
-            {/* PASSO */}
-            <StepIcon stepNumber={0} text={t("advertisements:add_advert.step", { count: 1 })} />
+            {steps.map((stepNumber) => (
+              <React.Fragment key={stepNumber}>
+                {/* PASSO */}
+                <StepIcon stepNumber={stepNumber} text={t("advertisements:add_advert.step", { count: stepNumber  })} />
+                <div
+                  className={classNames("flex-auto border-t-2", {
+                    "border-terciary-500": currentStep < stepNumber,
+                    "border-primary-500": currentStep >= stepNumber,
+                  })}
+                ></div>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="pb-4 pt-8 lg:hidden">
+        <div>
+          <div className="flex items-center justify-center">
+            <div className="flex-auto border-t-2 border-primary-500"></div>
+            {chunk[chunkIndex].map((stepNumber) => (
+              <React.Fragment key={stepNumber}>
+                {/* PASSO */}
+                <StepIcon stepNumber={stepNumber} text={t("advertisements:add_advert.step", { count: stepNumber  })} />
+                <div
+                  className={classNames("flex-auto border-t-2", {
+                    "border-terciary-500": currentStep < stepNumber,
+                    "border-primary-500": currentStep >= stepNumber,
+                  })}
+                ></div>
+              </React.Fragment>
+            ))}
+            <MdKeyboardDoubleArrowRight />
             <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 1,
-                "border-primary-500": currentStep >= 1,
-              })}
+                  className="flex-auto border-t-2 border-terciary-500"
             ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={1} text={t("advertisements:add_advert.step", { count: 2 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 2,
-                "border-primary-500": currentStep >= 2,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={2} text={t("advertisements:add_advert.step", { count: 3 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 3,
-                "border-primary-500": currentStep >= 3,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={3} text={t("advertisements:add_advert.step", { count: 4 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 4,
-                "border-primary-500": currentStep >= 4,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={4} text={t("advertisements:add_advert.step", { count: 5 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 5,
-                "border-primary-500": currentStep >= 5,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={5} text={t("advertisements:add_advert.step", { count: 6 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 6,
-                "border-primary-500": currentStep >= 6,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={6} text={t("advertisements:add_advert.step", { count: 7 })} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 7,
-                "border-primary-500": currentStep >= 7,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={7} text={t("advertisements:add_advert.step", {count: 8})} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 8,
-                "border-primary-500": currentStep >= 8,
-              })}
-            ></div>
-
-            {/* PASSO */}
-            <StepIcon stepNumber={8} text={t("advertisements:add_advert.step", {count: 9})} />
-            <div
-              className={classNames("flex-auto border-t-2", {
-                "border-terciary-500": currentStep < 9,
-                "border-primary-500": currentStep >= 9,
-              })}
-            ></div>
+              <React.Fragment >
+                {/* PASSO */}
+                <StepIcon stepNumber={10} text={t("advertisements:add_advert.step", { count: 10  })} />
+                <div
+                 className="flex-auto border-t-2 border-terciary-500"
+                ></div>
+              </React.Fragment>
           </div>
         </div>
       </div>
@@ -114,19 +90,19 @@ interface StepIconProps {
  */
 const StepIcon = ({ stepNumber, text }: StepIconProps) => {
   const currentStep = useCurrentStep();
-
   return (
     <div className="relative flex items-center text-white">
       <div
         className={classNames("h-10 w-10 rounded-full py-2 text-center", {
+          "bg-black": stepNumber === 10,
           "bg-primary-500": currentStep >= stepNumber,
-          "bg-terciary-200": stepNumber > currentStep,
+          "bg-terciary-200": stepNumber > currentStep && stepNumber < 10
         })}
       >
         &#10004;
       </div>
       <div className={`absolute top-0 -ml-10 mt-14 w-32 text-center text-xs font-medium uppercase ${
-       currentStep >= stepNumber ? "text-black" : "text-terciary-200"}`}>
+       currentStep >= stepNumber ||stepNumber === 10 ? "text-black" : "text-terciary-200"}`}>
         {text}
       </div>
     </div>
