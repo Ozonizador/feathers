3524fc29c36573f2ec7f981b2ab349397513b239
Rafael Norte <rnrfnorte@hotmail.com>
2023-12-18 17:58:50 +0000

    changed guests links to profile

--- components/hospedes/HospedeCard/HospedeCard.tsx
+++ components/hospedes/HospedeCard/HospedeCard.tsx
@@ -25,7 +25,7 @@ const HospedeCard = ({ stay }: HospedeCardProps) => {
         <h2 className="mb-6 mt-2 text-base text-secondary-500">
           {t(TYPE_ADVERTISEMENT[advertisement?.type])} {t("in")} {advertisement?.place}
         </h2>
-        <Link href={`/anuncio/${advertisement?.slug}`}>
+        <Link href={`/perfil/${tenant.slug}`}>
           <p className="text-base font-normal text-primary-500">+ {t("informations")}</p>
         </Link>
       </div>
