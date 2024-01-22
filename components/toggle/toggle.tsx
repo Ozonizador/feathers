9fbaca1d2427eeed096a8594587fb0ca59142cff
Rafael Norte <rnrfnorte@hotmail.com>
2024-01-22 14:31:41 +0000

    faqs toggle fix

--- components/toggle/toggle.tsx
+++ components/toggle/toggle.tsx
@@ -11,14 +11,14 @@ export default function Toggle({ selectedValue, onChange }: ToggleProps) {
   const { t } = useTranslation();
   return (
     <div className="mx-auto mb-20 py-10">
-      <div className="relative flex h-14 w-80 items-center rounded-full bg-primary-100 p-1 lg:h-16 lg:w-96">
+      <div className="relative flex w-80 items-center rounded-full bg-primary-100 p-1 lg:w-96">
         <div
           className={classNames("z-50 h-full flex-1 rounded-full", {
             "bg-primary-300 text-white": selectedValue == "TENANT",
           })}
           onClick={() => onChange("TENANT")}
         >
-          <h6 className="pt-3 text-center lg:pt-5">{t("student_one")}</h6>
+          <h6 className="py-3 text-center lg:py-5">{t("student_one")}</h6>
         </div>
         <div
           className={classNames("z-50 h-full flex-1 rounded-full", {
@@ -26,7 +26,7 @@ export default function Toggle({ selectedValue, onChange }: ToggleProps) {
           })}
           onClick={() => onChange("LANDLORD")}
         >
-          <h6 className="pt-3 text-center lg:pt-5">{t("landlord_one")}</h6>
+          <h6 className="py-3 text-center lg:py-5">{t("landlord_one")}</h6>
         </div>
       </div>
     </div>
