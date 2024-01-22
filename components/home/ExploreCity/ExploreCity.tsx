c93965f9965a35c969367bf447f047de649645a7
Rafael Norte <rnrfnorte@hotmail.com>
2024-01-22 13:32:12 +0000

    Fixes of images and how it works

--- components/home/ExploreCity/ExploreCity.tsx
+++ components/home/ExploreCity/ExploreCity.tsx
@@ -12,42 +12,42 @@ type CityProps = {
 const CONFIG = [
   {
     title: "Tomar",
-    image: "/images/place-tomar.jpg",
+    image: "/images/tomar.webp",
     url: "/procurar?city=Tomar",
   },
   {
     title: "Peniche",
-    image: "/images/peniche.jpg",
+    image: "/images/peniche.webp",
     url: "/procurar?city=Peniche",
   },
   {
     title: "Leiria",
-    image: "/images/place-leiria.jpg",
+    image: "/images/leiria.webp",
     url: "/procurar?city=Leiria",
   },
   {
     title: "Coimbra",
-    image: "/images/place-santarem.jpg",
+    image: "/images/coimbra.webp",
     url: "/procurar?city=Coimbra",
   },
   {
     title: "Abrantes",
-    image: "/images/place-abrantes.jpg",
+    image: "/images/abrantes.webp",
     url: "/procurar?city=Abrantes",
   },
   {
     title: "Rio Maior",
-    image: "/images/place-brazil.jpg",
+    image: "/images/riomaior.webp",
     url: `/procurar?city=Rio Maior`,
   },
   {
     title: "Porto",
-    image: "/images/place-braga.jpg",
+    image: "/images/porto.webp",
     url: "/procurar?city=Porto",
   },
   {
     title: "Caldas da Rainha",
-    image: "/images/place-setubal.jpg",
+    image: "/images/caldas.webp",
     url: "/procurar?city=Caldas da Rainha",
   },
 ] as CityProps[];
@@ -75,7 +75,7 @@ const ExploreCityItem = ({ title, url, image }: CityProps) => {
     <Link href={url}>
       <article className="group relative mx-auto h-60 w-60 cursor-pointer rounded-2xl bg-black lg:h-52 lg:w-52 ">
         <Image src={image} alt="tomar" fill className="rounded-2xl" />
-        <div className="break-none absolute flex h-full w-full flex-col justify-center align-middle text-2xl font-bold text-white transition-all group-hover:text-[55px] group-hover:leading-none group-hover:text-neutral-200 group-hover:opacity-40">
+        <div className="break-none bg-black bg-opacity-50 rounded-2xl absolute flex h-full w-full flex-col justify-center align-middle text-2xl font-bold text-white transition-all group-hover:text-[55px] group-hover:leading-none group-hover:text-neutral-200 group-hover:opacity-40">
           {title}
         </div>
       </article>
