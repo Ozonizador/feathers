dc1bc04d20807f296608d62e4e0bc6463ee40a74
Rafael Norte <rnrfnorte@hotmail.com>
2024-02-28 14:22:18 +0000

    bigger size

--- components/home/homeSection1/HomeSection1.tsx
+++ components/home/homeSection1/HomeSection1.tsx
@@ -7,7 +7,7 @@ const HomeSection1 = () => {
   const { t } = useTranslation();
   return (
     <section>
-      <div className="flex justify-center bg-[url('/images/furniture5.jpg')] bg-cover px-1 py-32 max-sm:py-16  min-w-[1660px]:py-40">
+      <div className="flex justify-center bg-[url('/images/furniture5.jpg')] bg-cover px-1 py-32 max-sm:py-16  2xl:py-48">
         <div className="lg:px-15 flex flex-col  px-4 align-middle md:container">
           <h1 className="mb-5 ml-2 text-4xl font-bold leading-snug  text-white lg:mx-0 lg:mb-0 lg:text-5xl">
             {t("index:yours")}{" "}
